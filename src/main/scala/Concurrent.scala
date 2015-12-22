62aef7dfca7d22867ef35da49d5705d9276d0dcc
Rafal Wolak <wolak.rafal@gmail.com>
2015-12-22 22:13:31 +0100

    Poor Man's Concurrency Monad . Implementation of action, fork, and _flatMap

--- src/main/scala/Concurrent.scala
+++ src/main/scala/Concurrent.scala
@@ -19,10 +19,33 @@ class Concurrent[A](val func: (A => Action) => Action) {
 
   def andThen[B](after: Concurrent[B]): Concurrent[B] = flatMap(_ => after)
 
+  // To convert 'func' to action we need stop work after function 'func' finishes work,
+  // So we need pass to 'func' an empty continuation function (that will do nothing and returns Stop)
   def action(): Action = func(_ => Stop)
 
-  def fork(): Concurrent[Unit] = ???
+  def fork(): Concurrent[Unit] =
+    Concurrent((cont: Unit => Action) => {
+      // turn function 'func' into an an action
+      val a1: Action = action()
+      // get action from continuation function by applying ()
+      val a2: Action = cont(())
+      Fork(a1, a2)
+    })
+
+  def _flatMap[B](f: (A => Action) => Action, g: A => ((B => Action) => Action)):
+      (B => Action) => Action = {
+
+    // :: (B-> Action) -> A -> Action
+    def lambda1(x : B => Action)(a : A): Action = g(a)(x)
+
+    // :: (B-> Action) -> Action
+    def lambda2(x : B => Action): Action = f(lambda1(x))
+
+    lambda2
+  }
+
   def flatMap[B](mapper: A => Concurrent[B]): Concurrent[B] = ???
+
 
   def run(): () => Unit = roundRobin(List[Action](action))
 }
@@ -31,9 +54,15 @@ object Concurrent {
   def apply[A](func: (A => Action) => Action) = new Concurrent[A](func)
   def of[A](a: A) = new Concurrent((cont: A => Action) => cont(a))
   
-  def stop[A](): Concurrent[A] = ???
-  def atom[A](ioA: Unit => A): Concurrent[A] = ???
+  def stop[A](): Concurrent[A] = Concurrent(_ => Stop)
+  def atom[A](ioA: Unit => A): Concurrent[A] = Concurrent((cont: A => Action) => cont(ioA()))
+
   def par[A](c1: Concurrent[A], c2: Concurrent[A]): Concurrent[A] = ???
+//    Concurrent((cont: A => Action) => {
+//      val a1: Fork = c1.fork().action()
+//      val a2: Action = c2.fork().action()
+//      Fork(a1,a2)
+//  })
 
   private def roundRobin(list: List[Action]): () => Unit = ???
 }
