fef2ae179323fe56596190f0208c96bdef617bab
Rafal Wolak <wolak.rafal@gmail.com>
2015-12-28 16:45:47 +0100

    Poor Man's Concurrency Monad . First complete implementation. Ready to evaluate

--- src/main/scala/Concurrent.scala
+++ src/main/scala/Concurrent.scala
@@ -33,36 +33,56 @@ class Concurrent[A](val func: (A => Action) => Action) {
     })
 
   def _flatMap[B](f: (A => Action) => Action, g: A => ((B => Action) => Action)):
-      (B => Action) => Action = {
+  (B => Action) => Action = {
 
     // :: (B-> Action) -> A -> Action
-    def lambda1(x : B => Action)(a : A): Action = g(a)(x)
+    def lambda1(x: B => Action)(a: A): Action = g(a)(x)
 
     // :: (B-> Action) -> Action
-    def lambda2(x : B => Action): Action = f(lambda1(x))
+    def lambda2(x: B => Action): Action = f(lambda1(x))
 
     lambda2
   }
 
-  def flatMap[B](mapper: A => Concurrent[B]): Concurrent[B] = ???
+  def flatMap[B](mapper: A => Concurrent[B]): Concurrent[B] = {
+    // :: (B-> Action) -> A -> Action
+    def lambda1(x: B => Action)(a: A): Action = mapper(a).func(x)
+
+    // :: (B-> Action) -> Action
+    def lambda2(x: B => Action): Action = func(lambda1(x))
+
+    Concurrent(lambda2)
+  }
 
 
-  def run(): () => Unit = roundRobin(List[Action](action))
+  def run(): () => Unit = roundRobin(List[Action](action()))
 }
 
 object Concurrent {
   def apply[A](func: (A => Action) => Action) = new Concurrent[A](func)
+
   def of[A](a: A) = new Concurrent((cont: A => Action) => cont(a))
-  
+
   def stop[A](): Concurrent[A] = Concurrent(_ => Stop)
+
   def atom[A](ioA: Unit => A): Concurrent[A] = Concurrent((cont: A => Action) => cont(ioA()))
 
-  def par[A](c1: Concurrent[A], c2: Concurrent[A]): Concurrent[A] = ???
-//    Concurrent((cont: A => Action) => {
-//      val a1: Fork = c1.fork().action()
-//      val a2: Action = c2.fork().action()
-//      Fork(a1,a2)
-//  })
+  def par[A](c1: Concurrent[A], c2: Concurrent[A]): Concurrent[A] = c1.andThen(c2)
 
-  private def roundRobin(list: List[Action]): () => Unit = ???
+  private def roundRobin(list: List[Action]): () => Unit = list match {
+    case Nil => stop().run() // nothnig to do
+    case x :: xs => x match {
+      case Stop => roundRobin(xs) // this computation is completed, go with other
+
+      case Atom(a) =>
+        val res = a.apply() // monadically execute computation,
+        // and put resulting process at the back of the process list.
+        roundRobin(xs :+ res)
+
+      case Fork(a1, a2) =>
+        // Fork creates two new processes
+        roundRobin(List(a1, a2) ++ xs)
+    }
+  }
+
 }
